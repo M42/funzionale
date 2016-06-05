30267da46639d2057c468d2804e5deb0164e03ce
M42 <mromang08@gmail.com>
2016-06-05 17:57:08 +0200

    Sequenze (Esercizio 1)

--- 14.sequenze.fsx
+++ 14.sequenze.fsx
@@ -1,7 +1,7 @@
 module Sequence
 
-let naturals = Seq.initInfinite (fun x -> x);;
-
+// ESERCIZI DI SEQUENZE 1
+// 1. Definire map
 let rec map f sq = seq{
     let e   = Seq.item 0 sq
     let sqe = Seq.skip 1 sq
@@ -9,7 +9,41 @@ let rec map f sq = seq{
     yield! (map f sqe)
     };;
 
-let rec naturals1 = seq{
-    yield 0
-    yield! map (fun x -> x+1) naturals1
+// 2. Aplicare alla sequenza dei naturalli per generare "squares"
+let naturals = Seq.initInfinite (fun x -> x);;
+let squares = map (fun x -> x*x) naturals;;
+
+// 3. Considerare il caso di sequenza vuota
+let rec map2 f sq = seq{
+    let empty = Seq.isEmpty sq 
+    
+    if (not empty) then
+      let e = Seq.item 0 sq
+      let sqe = Seq.skip 1 sq
+      yield (f e)
+      yield! (map2 f sqe)
     }
+;;
+
+// 4. Definire filter
+let rec filter pred sq = seq{
+    let e = Seq.item 0 sq
+    let sq1 = Seq.skip 1 sq
+    if pred e then
+      yield e
+    yield! (filter pred sq1)
+    }
+;;
+
+
+// 5. Multipli di 3
+let multipli = filter (fun x -> x%3 = 0) naturals;;
+
+// 6. Fibonacci
+let rec fibfrom a b = seq {
+    yield a
+    yield! (fibfrom b (a+b))
+    }
+
+let fib n = Seq.item n (fibfrom 0UL 1UL)
+
