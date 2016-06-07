1c8de535636a9a40304fdb085ab62645c3bc9699
M42 <mromang08@gmail.com>
2016-06-07 20:38:29 +0200

    Handling exceptions

--- 19.typecheck.fsx
+++ 19.typecheck.fsx
@@ -4,7 +4,7 @@ module TypeCheck
 // There are two types, integers and lists of integers
 type tp = INT | LSINT
 
-// A expression has the form:
+// An expression has the form:
 //  K - integer constant
 //  Plus - sum of integers
 //  Cons - list, given head and tail
@@ -22,13 +22,13 @@ type exp =
 // Typechecks recursively
 let rec tpck (expr:exp) =
     match expr with
-        | K ->
+        | K _ ->
             Some INT
         | Plus(a,b) ->
-            if tpck(a) = Some INT and tpck(b) = Some INT
+            if tpck(a) = Some INT && tpck(b) = Some INT
             then Some INT else None
         | Cons(h,t) ->
-            if tpck(h) = Some INT and tpck(t) = Some LSINT
+            if tpck(h) = Some INT && tpck(t) = Some LSINT
             then Some LSINT else None
         | Head(l) ->
             if tpck(l) = Some LSINT
@@ -38,3 +38,43 @@ let rec tpck (expr:exp) =
             then Some LSINT else None
         | Nil ->
             Some LSINT
+
+// Showing the type of an expression
+let showtype (expr:exp) =
+    match tpck expr with
+        | Some t -> printf "%A has type %A\n" exp t
+        | None -> printf "%A is not typable" exp
+        
+
+// Handling exceptions
+// First draft: in order to create a good typing system, we should
+// write more detailed exceptions
+exception TypeError of (exp * tp)
+
+let rec tpckf (expr:exp) =
+    match expr with
+        | K _ ->
+            INT
+        | Plus(a,b) ->
+            if tpckf(a) = INT && tpckf(b) = INT
+            then INT else raise(TypeError(expr, INT))
+        | Cons(h,t) ->
+            if tpckf(h) = INT && tpckf(t) = LSINT
+            then LSINT else raise(TypeError(expr, INT))
+        | Head(l) ->
+            if tpckf(l) = LSINT
+            then INT else raise(TypeError(expr, LSINT))
+        | Tail(l) ->
+            if tpckf(l) = LSINT
+            then LSINT else raise(TypeError(expr, LSINT))
+        | Nil ->
+            LSINT    
+
+let main (expr:exp) =
+    try
+        let t = tpckf(expr)
+        printf "%A has type %A\n" expr t
+    with
+    | TypeError(expe,tpe) ->
+        printf "Error typing %A with %A\n" expe tpe
+;;
