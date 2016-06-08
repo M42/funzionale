7d70dfd369934cac5c91141b3b82237cf22d6624
M42 <mromang08@gmail.com>
2016-06-08 13:21:02 +0200

    Minor changes

--- 16.testing.fsx
+++ 16.testing.fsx
@@ -10,25 +10,23 @@ let fizzbuzz = function
     | d             -> ""
     ;;
 
-
-
-
-
 // Checking simple tests
 // It passes all tests, but how many tests are have been passed only by the
 // use of the condition.
 let ``fizz if 3-multiple`` d =
-    (d>0 && d%3=0 && d%15<>0) ==> (fizzbuzz d = "fizz")
-;;
-    
+    //(d>0 && d%3=0 && d%15<>0) ==>
+      (fizzbuzz d = "fizz")
+Check.Quick(``fizz if 3-multiple``);;
+
 // A specific generator will do a more precise job
 type FizzBuzzTest =
     //static member positiveIntegers =
     //    Arb.Default.Int32()
     //    |> Arb.mapFilter abs (fun n -> n>0)
-
     static member fizzIntegers =
         Arb.Default.Int32()
         |> Arb.filter (fun n -> n>0 && n%3=0 && n%15<>0)
+
+
 
 Arb.registerByType(typeof<FizzBuzzTest>)
